c41c3ee6c771a55d693028fa31d0c5bb79b85c24
Nikko Ortega <NFCOrtega89@gmail.com>
2016-10-17 19:41:02 -0700

    update with test

--- server.js
+++ server.js
@@ -4,37 +4,48 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
+// create a object called Storage
 var Storage = {
+    //  a method to add an item with two properties name and id
     add: function(name) {
         var item = { name: name, id: this.setId };
+        // we want to push every item we add into the array
         this.items.push(item);
+        // increment the id number by 1 after every item added
         this.setId += 1;
         return item;
     },
+    // a method to remove an item
     remove: function(id) {
-
         // find the "item"'s index whose id is "id"
         for (var index = 0; index < this.items.length; index++) {
+            // check to see if the id is the same as the current id in the loop
             if (id == this.items[index].id) {
+                // extract that item by using the splice method
                 this.items.splice(index, 1);
                 break;
             }
         }
         return id;
     },
+    // method to edit an item
     edit: function(id, name) {
-        // editin stuff! dah!
+        // find the "items"'s index whose id is "id"'
         for (var index = 0; index < this.items.length; index++) {
             if (id == this.items[index].id) {
+                // here we change the items name to the current name;
                 this.items[index].name = name;
-                return this.items[index]; // return "item" when successful found
+                // return "item" when successfully found
+                return this.items[index];
             }
         }
-        return {}; // when nothing happens or not found.
-
+        // when nothing happens or not found.
+        return {};
     }
 };
 
+// we have a function that creates a storage object that uses our "Storage" function
+// we'll give storage 2 properties. Empty items array & setId
 var createStorage = function() {
     var storage = Object.create(Storage);
     storage.items = [];
@@ -42,15 +53,19 @@ var createStorage = function() {
     return storage;
 }
 
+// create a variable called storage where we use createStorage
 var storage = createStorage();
 
 storage.add('Broad beans');
 storage.add('Tomatoes');
 storage.add('Peppers');
 
+
 var app = express();
+// tells express to server any static content contained in the public folder
 app.use(express.static('public'));
 
+// we use the get request on app and pass in a callback function taking request and response
 app.get('/items', function(request, response) {
     response.json(storage.items);
 })
@@ -77,5 +92,6 @@ app.put('/items/:id', jsonParser, function(request, response) {
 })
 app.listen(process.env.PORT || 8080);
 
+// export both app and storage
 exports.app = app;
 exports.storage = storage;
--- test/test-server.js
+++ test/test-server.js
@@ -1,3 +1,4 @@
+// we require the chai, chai-http and the private module
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../server.js');
@@ -9,8 +10,41 @@ var storage = server.storage;
 chai.use(chaiHttp);
 
 describe('Shopping List', function() {
-    it('should list items on get');
-    it('should add an item on post');
-    it('should edit an item on put');
-    it('should delete an item on delete');
+    it('should list items on get', function(done){
+    	chai.request(app)
+    	.get('/items')
+    	.end(function(err, res){
+    		res.should.have.status(200);
+    		done();
+    	});
+    });
+    it('should add an item on post', function(done){
+    	chai.request(app)
+    	.post('/items')
+    	.send({name: 'Kale'})
+    	.end(function(err, res){
+    		should.equal(err, null); // no error!!! 
+    		res.should.have.status(201);
+    		done();
+    	})
+    });
+    it('should edit an item on put', function(done){
+    	chai.request(app)
+    	.put('/items/4')
+    	.send({name: 'Potatoes'})
+    	.end(function(err, res){
+    		storage.items[3].name.should.equal('Potatoes');
+    		res.should.have.status(201);
+    		done();
+    	})
+    });
+    it('should delete an item on delete', function(done){
+    	chai.request(app)
+    	.delete('/items/4')
+    	.end(function(err, res){
+    		storage.items.should.not.have.length.above(3)
+    		res.should.have.status(201);
+    		done();
+    	})
+    });
 });
